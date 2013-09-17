bc65ada9515bb998861a215f16de98e4e4e0fcd8
Robert Haines <rhaines@manchester.ac.uk>
2013-09-17 15:55:56 +0100

    Remove moribund TavernaPlayer::Workflow model.
    It was empty and not being used anyway. Going to use a function mapper instead.

--- app/models/taverna_player/workflow.rb
+++
@@ -1,7 +0,0 @@
-module TavernaPlayer
-  class Workflow < TavernaPlayer.workflow_class
-
-    # Not sure if we need this...
-    #set_table_name TavernaPlayer.workflow_class.to_s.downcase.pluralize
-  end
-end
