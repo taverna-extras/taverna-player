608194b6f69159423f47cc5a5d5fefb8fde934ba
Robert Haines <rhaines@manchester.ac.uk>
2013-09-23 15:05:24 +0100

    Expose TavernaPlayer::ApplicationHelper methods in the main app.
    So we can use the new_embedded_run helpers in the main app.
    
    This is now used in the dummy homepage as an example.

--- lib/taverna_player/engine.rb
+++ lib/taverna_player/engine.rb
@@ -1,5 +1,11 @@
 module TavernaPlayer
   class Engine < ::Rails::Engine
     isolate_namespace TavernaPlayer
+
+    initializer "taverna_player.action_controller" do
+      ActiveSupport.on_load :action_controller do
+        helper TavernaPlayer::ApplicationHelper
+      end
+    end
   end
 end
