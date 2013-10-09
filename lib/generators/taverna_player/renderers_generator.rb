060bb50a30106604d9bd4959c466f8e0109c521b
Robert Haines <rhaines@manchester.ac.uk>
2013-10-09 15:41:41 +0100

    Generator re-organization: Callbacks.
    Moved into their own callbacks directory.

--- lib/generators/taverna_player/renderers_generator.rb
+++ lib/generators/taverna_player/renderers_generator.rb
@@ -2,7 +2,7 @@
 module TavernaPlayer
   module Generators
     class RenderersGenerator < Rails::Generators::Base
-      source_root File.expand_path("../../templates", __FILE__)
+      source_root File.expand_path("../../templates/callbacks", __FILE__)
 
       desc "Create some basic renderer callbacks in "\
         "'lib/taverna_player_renderers.rb'"
