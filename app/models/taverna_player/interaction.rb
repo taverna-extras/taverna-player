4d33be28c48f9fa03f9b7d41336b910b42f144e6
Robert Haines <rhaines@manchester.ac.uk>
2013-10-16 18:08:45 +0100

    [TAV-374] Formalize the run/interaction association.
    This was already declared in one direction, so this closes the loop.

--- app/models/taverna_player/interaction.rb
+++ app/models/taverna_player/interaction.rb
@@ -3,6 +3,8 @@ module TavernaPlayer
     attr_accessible :displayed, :feed_reply, :output_value, :page, :replied,
       :unique_id
 
+    belongs_to :run, :class_name => "TavernaPlayer::Run"
+
     validates_presence_of :unique_id
     validates_uniqueness_of :unique_id
   end
