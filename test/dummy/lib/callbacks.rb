8686d48c2202ed09e053613a33f8d0bf8f85c8a3
Robert Haines <rhaines@manchester.ac.uk>
2013-11-25 14:16:37 +0000

    [TAV-438] Add licence text to test source files.

--- test/dummy/lib/callbacks.rb
+++ test/dummy/lib/callbacks.rb
@@ -1,4 +1,14 @@
-# Dummy callbacks
+#------------------------------------------------------------------------------
+# Copyright (c) 2013 The University of Manchester, UK.
+#
+# BSD Licenced. See LICENCE.rdoc for details.
+#
+# Taverna Player was developed in the BioVeL project, funded by the European
+# Commission 7th Framework Programme (FP7), through grant agreement
+# number 283359.
+#
+# Author: Robert Haines
+#------------------------------------------------------------------------------
 
 def player_pre_run_callback(run)
   w = Workflow.find(run.workflow_id)
