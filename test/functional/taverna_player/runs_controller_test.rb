3bee54f171b7faa8c9aa17a98e2624bb9488c9e2
Robert Haines <rhaines@manchester.ac.uk>
2014-01-02 18:02:33 +0000

    Update various copyright headers.
    Changed files should now say 2014...

--- test/functional/taverna_player/runs_controller_test.rb
+++ test/functional/taverna_player/runs_controller_test.rb
@@ -1,5 +1,5 @@
 #------------------------------------------------------------------------------
-# Copyright (c) 2013 The University of Manchester, UK.
+# Copyright (c) 2013, 2014 The University of Manchester, UK.
 #
 # BSD Licenced. See LICENCE.rdoc for details.
 #
