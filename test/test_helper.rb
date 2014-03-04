9be05aa00b96064d072316bc0604ab1d9af29d3b
Finn Bacall <finn.bacall@manchester.ac.uk>
2014-03-04 10:32:58 +0000

    Fixed issue with loading fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -25,9 +25,7 @@ Rails.backtrace_cleaner.remove_silencers!
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 # Load fixtures from the engine
-if ActiveSupport::TestCase.method_defined?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
-end
+ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
 
 class ActiveSupport::TestCase
   include TavernaPlayer::Engine.routes.url_helpers
