1d174bfc62f7562b7302a3e6f84a31a8b03df48e
Robert Haines <rhaines@manchester.ac.uk>
2014-09-18 11:16:54 +0100

    Allow extending of the internal Workflow model.
    Turn it into a concern like the other models.

--- app/models/taverna_player/workflow.rb
+++ app/models/taverna_player/workflow.rb
@@ -12,25 +12,30 @@
 
 module TavernaPlayer
 
+  # This class represents a workflow. It is provided as an example model and
+  # can be extended or replaced as required.
+  #
+  # To replace the model completely please read about configuring the workflow
+  # model proxy in the "Taverna Player initializers" section of the ReadMe. To
+  # extend the model please read about "Overriding the default models and
+  # controllers" in the ReadMe.
   class Workflow < ActiveRecord::Base
-    attr_accessible :author, :description, :file, :title
+    include TavernaPlayer::Concerns::Models::Workflow
 
-    has_many :runs, :class_name => "TavernaPlayer::Run"
-
-    def inputs
-      workflow = File.open(file)
-      model = T2Flow::Parser.new.parse(workflow)
-
-      result = []
-      model.sources.each do |i|
-        description = i.descriptions.nil? ? "" : i.descriptions.join
-        example = i.example_values.nil? ? "" : i.example_values.join
-        result << { :name => i.name, :description => description,
-          :example => example }
-      end
-
-      result
-    end
+    ##
+    # :method: inputs
+    # :call-seq:
+    #   inputs -> Hash
+    #
+    # Return a hash of information about this workflow's inputs. The fields
+    # provided are:
+    # * <tt>:name</tt> - The name of the input port.
+    # * <tt>:description</tt> - A textual description (if provided) of the
+    #   input port.
+    # * <tt>:example</tt> - Example input data (if provided) for the input
+    #   port.
+    #
+    # All data is read directly from the workflow file itself.
 
   end
 end
