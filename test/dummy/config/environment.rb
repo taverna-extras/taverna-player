c0ecba14985d98c42ebbfd7a6dc35965fe1b3228
Finn Bacall <finn.bacall@manchester.ac.uk>
2015-06-25 09:37:02 +0100

    Fixed Ruby 1.9.3 encoding issue

--- test/dummy/config/environment.rb
+++ test/dummy/config/environment.rb
@@ -1,11 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
-
 # Initialize the rails application
 Dummy::Application.initialize!
-
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
